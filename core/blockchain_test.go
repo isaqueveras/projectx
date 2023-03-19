8e2d0c495da0d53972aaa476f1ebaba6e695fa76
Isaque Véras <isaqueveras@alu.ufc.br>
2023-03-19 10:56:14 -0300

    feat: creating the blockchain data structure

---
+++ core/blockchain_test.go
@@ -0,0 +1,38 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func newBlockchainWithGenesis(t *testing.T) *Blockchain {
+	bc, err := NewBlockchain(randomBlock(0))
+	assert.Nil(t, err)
+	return bc
+}
+
+func TestNewBlockchain(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+	assert.NotNil(t, bc.validator)
+	assert.Equal(t, bc.Height(), uint32(0))
+}
+
+func TestHasBlock(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+	assert.True(t, bc.HasBlock(0))
+}
+
+func TestAddBlock(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+
+	lenBlocks := uint32(1000)
+	for i := 0; i < int(lenBlocks); i++ {
+		block := randomBlockWithSignature(t, uint32(i+1))
+		assert.Nil(t, bc.AddBlock(block))
+	}
+
+	assert.Equal(t, bc.Height(), lenBlocks)
+	assert.Equal(t, uint32(len(bc.headers)), lenBlocks+1)
+	assert.NotNil(t, bc.AddBlock(randomBlock(uint32(89))))
+}
